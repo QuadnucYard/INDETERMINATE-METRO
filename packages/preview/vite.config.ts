3228e0997edc8230c84c3276f5a9b0a3cda6fa48
QuadnucYard <quadnucyard@gmail.com>
2025-12-09 10:02:05 +0800

    build: migrate to vite 8 and typescript-go

--- packages/preview/vite.config.ts
+++ packages/preview/vite.config.ts
@@ -2,4 +2,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/INDETERMINATE-METRO/",
+
+  build: {
+    rolldownOptions: {
+      output: {
+        advancedChunks: {
+          groups: [{ name: "three", test: /\/three/ }],
+        },
+      },
+    },
+  },
 });
